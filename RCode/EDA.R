de3a88d43f53fb5ecf564d89409a710ad89ee153
Ben Zhang <nebgnahz@gmail.com>
2014-04-27 14:21:10 -0700

    initialization with code checked in

---
+++ RCode/EDA.R
@@ -0,0 +1,38 @@
+data <- read.csv("http://galaxy.eecs.berkeley.edu:8000/data1.csv")
+
+acc <- data[data$type == "accelerometer",]
+acc[acc$id == "276dd3d0-fda1-31a8-9a74-8764e9d2a75e", 4] <-
+  acc[acc$id == "276dd3d0-fda1-31a8-9a74-8764e9d2a75e", 4] - mean(acc[acc$id == "276dd3d0-fda1-31a8-9a74-8764e9d2a75e", 4])
+
+acc[acc$id == "9026086e-bd07-3f96-9622-757da2907a93", 4] <-
+  acc[acc$id == "9026086e-bd07-3f96-9622-757da2907a93", 4] - mean(acc[acc$id == "9026086e-bd07-3f96-9622-757da2907a93", 4])
+
+ggplot(acc, aes(x = sysnano, y = z)) + geom_point() + facet_grid(. ~ id)
+
+
+## > 
+##                    type                                          id       
+##  gyroscope           :24323   276dd3d0-fda1-31a8-9a74-8764e9d2a75e:32621  
+##  accelerometer       :13559   9026086e-bd07-3f96-9622-757da2907a93:62968  
+##  magnetic            :13552                                               
+##  gravity             :13457                                               
+##  linear accelerometer:13457                                               
+##  rotation            :13455                                               
+##  (Other)             : 3786                                               
+##    eventnano            sysnano                x                 y          
+##  Min.   :1.033e+13   Min.   :1.033e+13   Min.   :-55.669   Min.   :-45.171  
+##  1st Qu.:1.037e+13   1st Qu.:1.037e+13   1st Qu.: -0.219   1st Qu.: -0.081  
+##  Median :1.043e+13   Median :1.043e+13   Median :  0.020   Median :  0.072  
+##  Mean   :1.509e+14   Mean   :1.587e+14   Mean   :  0.361   Mean   :  1.270  
+##  3rd Qu.:4.448e+14   3rd Qu.:4.448e+14   3rd Qu.:  0.651   3rd Qu.:  9.393  
+##  Max.   :4.451e+14   Max.   :4.451e+14   Max.   : 36.089   Max.   : 19.613  
+##  NA's   :3607                            NA's   :17241     NA's   :17241    
+##        z                 xr              yr              zr       
+##  Min.   :-52.307   Min.   :-0.19   Min.   :-0.82   Min.   :-0.97  
+##  1st Qu.: -0.028   1st Qu.: 0.00   1st Qu.:-0.58   1st Qu.:-0.63  
+##  Median :  0.082   Median : 0.10   Median : 0.16   Median : 0.27  
+##  Mean   :  1.332   Mean   : 0.20   Mean   : 0.02   Mean   : 0.12  
+##  3rd Qu.:  0.979   3rd Qu.: 0.35   3rd Qu.: 0.68   3rd Qu.: 0.71  
+##  Max.   : 30.604   Max.   : 0.84   Max.   : 0.75   Max.   : 0.97  
+##  NA's   :17241     NA's   :82134   NA's   :82134   NA's   :82134  
+## > 
---
+++ README.md
@@ -0,0 +1,9 @@
+# _Project AR: A comparison study of sensor placement for Activity Recognition_
+
+_Description: This is the repository for [CS281A](http://www.cs.berkeley.edu/~jordan/courses/281A-spring14/)_
+
+## Introduction
+A high-level goal of this project is to evaluate how activity recoginition algorithms perform with different sensor positions. Previous research has primarily focused on using mobile phone as the platform for activity recoginition. We argue that with the emerging wearable technology, different tasks will be offloaded to sensors that are place at different positions. Specifically for activity recoginition, sensors on Glass might perform better than sensors in mobile phone for certain gestures.
+
+In this project, we seek to quantify the difference.
+
