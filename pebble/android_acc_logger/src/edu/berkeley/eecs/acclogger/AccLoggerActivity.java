a534d506e65b331c3d7a22e90707043211596bc9
Ben Zhang <benzh@eecs.berkeley.edu>
2014-05-03 15:21:36 -0700

    added Android code and also update gitignroe in the subfolder

---
+++ pebble/android_acc_logger/src/edu/berkeley/eecs/acclogger/AccLoggerActivity.java
@@ -0,0 +1,15 @@
+package edu.berkeley.eecs.acclogger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccLoggerActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
