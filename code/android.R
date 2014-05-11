91d05d70fdd67284962d82abc19d83aba27a1b98
Yuxun <yxzhou@berkeley.edu>
2014-05-10 18:41:59 -0700

    seperate feature selection and learning, source("android.R") for all features

--- code/android.R
+++ code/android.R
@@ -31,12 +31,12 @@ glass_plot <- ggplot(glass, aes(x = posixlt, y = x)) + geom_point()
 phone_acc <- phone[phone$type == "accelerometer",]
 glass_acc<- glass[glass$type == "accelerometer",]
 
-phone_gyro<- phone[phone$type == "gyroscope",]
-glass_gyro <- glass[glass$type == "gyroscope",]
+# phone_gyro<- phone[phone$type == "gyroscope",]
+# glass_gyro <- glass[glass$type == "gyroscope",]
 
-par(mfrow = c(2, 1))
-plot(1:length(phone$ts2), phone$ts2)
-plot(1:length(glass$ts2), glass$ts2)
+# par(mfrow = c(2, 1))
+# plot(1:length(phone$ts2), phone$ts2)
+# plot(1:length(glass$ts2), glass$ts2)
 
 ## multiplot(x_axis, y_axis, z_axis, phone_acc_plot)
 glass_acc_plot <- ggplot(glass_acc, aes(x = posixlt, y = y)) + geom_point() +
@@ -72,41 +72,14 @@ phone_dat <- phone_acc[phone_acc$label!=0,]
 ### below should be updated
 
 acc <- data[data$type == "accelerometer",]
-gyro <- data[data$type == "gyroscope",]
-rotation <- data[data$type == "rotation",]
+# gyro <- data[data$type == "gyroscope",]
+# rotation <- data[data$type == "rotation",]
+# 
+# ggplot(acc, aes(x = sysnano, y = z)) + geom_point() + facet_grid(. ~ id)
+# ggplot(gyro, aes(x = sysnano, y = x)) + geom_point() + facet_grid(. ~ id)
+# ggplot(rotation, aes(x = sysnano, y = zr)) + geom_point() + facet_grid(. ~ id)
+# ggplot(light, aes(x = sysnano, y = zr)) + geom_point() + facet_grid(. ~ id)
 
-ggplot(acc, aes(x = sysnano, y = z)) + geom_point() + facet_grid(. ~ id)
-ggplot(gyro, aes(x = sysnano, y = x)) + geom_point() + facet_grid(. ~ id)
-ggplot(rotation, aes(x = sysnano, y = zr)) + geom_point() + facet_grid(. ~ id)
-ggplot(light, aes(x = sysnano, y = zr)) + geom_point() + facet_grid(. ~ id)
-
-
-## > 
-##                    type                                          id       
-##  gyroscope           :24323   276dd3d0-fda1-31a8-9a74-8764e9d2a75e:32621  
-##  accelerometer       :13559   9026086e-bd07-3f96-9622-757da2907a93:62968  
-##  magnetic            :13552                                               
-##  gravity             :13457                                               
-##  linear accelerometer:13457                                               
-##  rotation            :13455                                               
-##  (Other)             : 3786                                               
-##    eventnano            sysnano                x                 y          
-##  Min.   :1.033e+13   Min.   :1.033e+13   Min.   :-55.669   Min.   :-45.171  
-##  1st Qu.:1.037e+13   1st Qu.:1.037e+13   1st Qu.: -0.219   1st Qu.: -0.081  
-##  Median :1.043e+13   Median :1.043e+13   Median :  0.020   Median :  0.072  
-##  Mean   :1.509e+14   Mean   :1.587e+14   Mean   :  0.361   Mean   :  1.270  
-##  3rd Qu.:4.448e+14   3rd Qu.:4.448e+14   3rd Qu.:  0.651   3rd Qu.:  9.393  
-##  Max.   :4.451e+14   Max.   :4.451e+14   Max.   : 36.089   Max.   : 19.613  
-##  NA's   :3607                            NA's   :17241     NA's   :17241    
-##        z                 xr              yr              zr       
-##  Min.   :-52.307   Min.   :-0.19   Min.   :-0.82   Min.   :-0.97  
-##  1st Qu.: -0.028   1st Qu.: 0.00   1st Qu.:-0.58   1st Qu.:-0.63  
-##  Median :  0.082   Median : 0.10   Median : 0.16   Median : 0.27  
-##  Mean   :  1.332   Mean   : 0.20   Mean   : 0.02   Mean   : 0.12  
-##  3rd Qu.:  0.979   3rd Qu.: 0.35   3rd Qu.: 0.68   3rd Qu.: 0.71  
-##  Max.   : 30.604   Max.   : 0.84   Max.   : 0.75   Max.   : 0.97  
-##  NA's   :17241     NA's   :82134   NA's   :82134   NA's   :82134  
-## > 
 
 norm_vec <- function(x) sqrt(sum(x^2))
 #calculate binned distribution
@@ -230,89 +203,11 @@ dat5 = cbind(y,feature5);
 dat_all_phone = rbind(dat1,dat2,dat3,dat4,dat5);
 dat_all_phone[,'y'] <- as.factor(dat_all_phone[,'y']); 
 
-# theme_set(theme_gray(base_size = 24))
-# #time domain feature distribution
-# pdf('./figures/edafeature1.pdf',width=10,height=7)
-# a1 = ggplot(dat_all_glass, aes(mean_x, fill=as.factor(y))) +geom_density(alpha = 0.2)
-# a2 = ggplot(dat_all_glass, aes(var_z, fill=as.factor(y))) +geom_density(alpha = 0.2)
-# a3 = ggplot(dat_all_glass, aes(magnitude, fill=as.factor(y))) +geom_density(alpha = 0.2)
-# a4 = ggplot(dat_all_glass, aes(diff_z, fill=as.factor(y))) +geom_density(alpha = 0.2)
-# multiplot(a1, a2, a3, a4,cols=2)
-# dev.off()
-# 
-# #frequency domain feature distribution
-# 
-# pdf('./281A/figures/edafeature2.pdf',width=10,height=7)
-# a1 = ggplot(dat_all_glass, aes(fft_x, fill=y)) +geom_density(alpha = 0.2) 
-# a2 = ggplot(dat_all_glass, aes(fft_z, fill=y)) +geom_density(alpha = 0.2)
-# a3 = ggplot(dat_all_glass, aes(entropy_x, fill=y)) +geom_density(alpha = 0.2)
-# a4 = ggplot(dat_all_glass, aes(entropy_z, fill=y)) +geom_density(alpha = 0.2)
-# multiplot(a1, a2, a3, a4, cols = 2)
-# dev.off()
-
 # matrix scatter plot
 
 plotmatrix(dat_all_glass[,c(2,3,15,18)], colour="gray20") + geom_smooth(method="lm")
 
-write.csv(dat_all_glass, file = 'actionglass.csv')
-# It's time for these quasi-reliable libraries
-library('nnet')
-library('foreign')
-library('reshape2')
-library('C50')
-library('e1071')
+write.csv(dat_all_glass, file = 'action_glass.csv')
 
-# multi-class logistic regression test
-dat_con_glass = dat_all_glass[,1:20]
-
-test1 <- multinom(y ~ ., data = dat_con_glass)
-pre1 = predict(test1,newdata=dat_con_glass[,2:20])
-sum(pre1==dat_con_glass$y)/length(pre1)
-table(pre1,dat_con_glass$y)
-
-dat_con_phone = dat_all_phone[,1:20]
-test1 <- multinom(y ~ ., data = dat_con_phone)
-pre1 = predict(test1,newdata=dat_con_phone[,2:20])
-sum(pre1==dat_con_phone$y)/length(pre1)
-table(pre1,dat_con_phone$y)
-
-
-
-# multi-class decision tree J48
-test2 <- J48( y ~ mean_x + var_z + magnitude , data= dat_all_glass, control= Weka_control(M=1,U=TRUE))
-
-# multi-class decision tree C4.5
-test3 <- C5.0(as.factor(y) ~ ., data = dat_con_glass)
-pre3 = predict(test3,newdata=dat_con_glass[,2:20])
-sum(pre3==dat_con_glass$y)/length(pre3)
-table(pre3,dat_con_glass$y)
-
-test3 <- C5.0(as.factor(y) ~ ., data = dat_con_phone)
-pre3 = predict(test3,newdata=dat_con_phone[,2:20])
-sum(pre3==dat_con_phone$y)/length(pre3)
-table(pre3,dat_con_phone$y)
-
-# multi-class svm 
-test4 <- svm(y ~ .,data=dat_con_glass)
-pre4 = predict(test4,newdata=dat_con_glass[,2:20])
-sum(pre4==dat_con_glass$y)/length(pre4)
-
-
-
-# go to matlab for dynamic bayesian network method
-library('HMM')
-library('bnlearn')
-library('depmixS4')  #this one looks better
-test5 <- depmix(list(mean_x~1,var_z~1,magnitude~1),data=dat_all,nstates=5,
-                family=list(gaussian(),gaussian(),gaussian()));
-fit_test5 <- fit(test5)
-summary(fit_test5,which="all")
-post5 <- posterior(fit_test5)
-param5 <- forwardbackward(fit_test5, return.all=TRUE, useC=TRUE)
-
-
-
-# regularized svm and logistic regression 
-library('LiblineaR')
 
 
