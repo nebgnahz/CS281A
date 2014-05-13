99479163efdf284fcd10e6447828459acf0caaf5
Yuxun <yxzhou@berkeley.edu>
2014-05-12 21:09:39 -0700

    Plot features in Main.R

--- code/main.R
+++ code/main.R
@@ -46,9 +46,34 @@ for (i in 1:length(labels)) {
 }
 
 dat_all[,'y'] <- as.factor(dat_all[,'y']); 
+dat_all$Index <- seq(1,length(dat_all$y))
 
 plotmatrix(dat_all[,c(2,3,15,18)], colour="gray20") + geom_smooth(method="lm")
 
+
+pdf('~/features.pdf',width=10,height=7)
+a1 <-ggplot(dat_all, aes(x = Index, y = mean_y)) +
+  geom_point( aes(colour = y,shape=y)) + 
+  theme(panel.background = element_rect(fill = 'white', colour = 'grey'))
+a2 <-ggplot(dat_all, aes(x = Index, y = var_x)) +
+  geom_point( aes(colour = y,shape=y)) + 
+  theme(panel.background = element_rect(fill = 'white', colour = 'grey'))
+a3 <-ggplot(dat_all, aes(x = Index, y = diff_x)) +
+  geom_point( aes(colour = y,shape=y)) + 
+  theme(panel.background = element_rect(fill = 'white', colour = 'grey'))
+a4 <-ggplot(dat_all, aes(x = Index, y = fft_z)) +
+  geom_point( aes(colour = y,shape=y)) + 
+  theme(panel.background = element_rect(fill = 'white', colour = 'grey'))
+a5 <-ggplot(dat_all, aes(x = Index, y = entropy_y)) +
+  geom_point( aes(colour = y,shape=y)) + 
+  theme(panel.background = element_rect(fill = 'white', colour = 'grey'))
+a6 <-ggplot(dat_all, aes(x = Index, y = cov_xy)) +
+  geom_point( aes(colour = y,shape=y)) + 
+  theme(panel.background = element_rect(fill = 'white', colour = 'grey'))
+
+multiplot(a1, a2, a3, a4, a5,a6,cols = 2)
+
+dev.off()
 # It's time for these quasi-reliable libraries
 library('nnet')
 library('foreign')
